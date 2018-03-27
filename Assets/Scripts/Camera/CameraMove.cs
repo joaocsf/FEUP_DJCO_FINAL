16fcb225c931cd9ab253accfc209f2c2351121c8
joaocsf <noobcsf@hotmail.com>
2018-03-27 19:00:11 +0100

    Added Prespective change

---
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+
+    Vector2 mouse = Vector2.zero;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        Vector3 movement = transform.forward * input.y
+                          + transform.right * input.x;
+
+        if (Input.GetKey(KeyCode.Space))
+            movement += Vector3.up;
+        else if(Input.GetKey(KeyCode.LeftShift))
+            movement -= Vector3.up;
+
+        transform.position += movement * Time.deltaTime * 10;
+
+        mouse.x += Input.GetAxisRaw("Mouse X");
+        mouse.y -= Input.GetAxisRaw("Mouse Y");
+
+        mouse.y = Mathf.Clamp(mouse.y, -90, 90);
+
+        transform.eulerAngles = new Vector3(mouse.y, mouse.x, 0);
+
+		
+	}
+}
---
+++ Assets/Scripts/Camera/ScreenCapture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenCapture : MonoBehaviour {
+
+    Camera cam;
+    public int resolution = 512;
+    Cubemap cubemap;
+    public Camera cam2;
+    public Material skybox;
+    float scale = 10;
+	// Use this for initialization
+	void Start () {
+
+        cam = GetComponent<Camera>();
+        RenderTexture rt = new RenderTexture(Screen.width, Screen.height, 0);
+        Shader.SetGlobalTexture("_Skybox", rt);
+        cam.targetTexture = rt;
+	}
+
+	void LateUpdate () {
+        cam.transform.localPosition = cam2.transform.localPosition / scale;
+        cam.transform.localEulerAngles = cam2.transform.localEulerAngles;
+	}
+}

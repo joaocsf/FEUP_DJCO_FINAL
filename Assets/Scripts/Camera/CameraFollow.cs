974fd83ba56e1e380e100b3c9049a02bf1217ab6
lazarocosta <lazaroosta@hotmail.com>
2018-04-26 23:42:04 +0100

    fixed merge

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
+public class CameraFollow : MonoBehaviour
+{
 
 
 
-	 public float sensitivity = 3;
+    public float sensitivity = 3;
 
-	 public float angle = 45f;
+    public float angle = 45f;
 
     public float radius = 3;
 
@@ -19,86 +20,97 @@ public class CameraFollow : MonoBehaviour {
     private Vector3 cameraPos;
     public Transform obj;
 
-		public float lerpVelocity = 10;
+    public float lerpVelocity = 10;
 
-		public bool collide = false;
+    public bool collide = false;
 
-		public Vector2 lastPressed = Vector2.zero;
+    public Vector2 lastPressed = Vector2.zero;
 
-	void Start () {
-		
-	}
+    void Start()
+    {
 
-	public void SetTransform(Transform obj){
-		this.obj = obj;
-	}
+    }
 
-	
-
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.Q))
-			lastPressed.x = 1;
-		else if(Input.GetKeyDown(KeyCode.E))
-			lastPressed.x = -1;
-		else
-			lastPressed.x = 0;
-		//mouse.x += Input.GetAxis("Mouse X")  * sensitivity;
-		//mouse.y -= Input.GetAxis("Mouse Y") * sensitivity;
-		mouse.x += lastPressed.x * angle;
-		mouse.x %= 360;
-		mouse.y = Mathf.Clamp(mouse.y, -80, 70);
-
-		radius -= Input.GetAxisRaw("Mouse ScrollWheel") * 2;
+    public void SetTransform(Transform obj)
+    {
+        this.obj = obj;
+    }
 
 
-		if (obj == null) return;
 
-		PositionCamera(obj, Time.deltaTime);
-		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
-	}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+            lastPressed.x = 1;
+        else if (Input.GetKeyDown(KeyCode.E))
+            lastPressed.x = -1;
+        else
+            lastPressed.x = 0;
+        //mouse.x += Input.GetAxis("Mouse X")  * sensitivity;
+        //mouse.y -= Input.GetAxis("Mouse Y") * sensitivity;
+        this.CameraRotate(lastPressed.x);
+        mouse.y = Mathf.Clamp(mouse.y, -80, 70);
 
-	public Vector3 GetPlaneDirection(Vector3 point){
-		Matrix4x4 matrix = 
-			Matrix4x4.Rotate(Quaternion.Euler(new Vector3(0,mouse.x,0)))
-			* Matrix4x4.identity;
+        radius -= Input.GetAxisRaw("Mouse ScrollWheel") * 2;
 
-		return matrix.MultiplyPoint(point);
-	}
 
-	public Vector3 GetDirection()
-	{
-		Matrix4x4 matrix = 
-			Matrix4x4.Rotate(Quaternion.Euler(new Vector3(0,mouse.x,0)))
-			* Matrix4x4.Rotate(Quaternion.Euler(new Vector3(mouse.y,0,0)))
-			* Matrix4x4.Translate(-Vector3.forward) 
-			* Matrix4x4.identity;
+        if (obj == null) return;
 
-		return matrix.MultiplyPoint(new Vector3(0,0,0));
-	}
+        PositionCamera(obj, Time.deltaTime);
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+    }
 
-	private void OnDrawGizmos()
-	{
-		if(obj == null) return;
-		Vector3 cameraDir = GetDirection();
-		Gizmos.color = Color.red;
-		Gizmos.DrawLine(obj.position, obj.position + cameraDir * radius);
-	}
+    public Vector3 GetPlaneDirection(Vector3 point)
+    {
+        Matrix4x4 matrix =
+            Matrix4x4.Rotate(Quaternion.Euler(new Vector3(0, mouse.x, 0)))
+            * Matrix4x4.identity;
 
-	public void ForcePositionCamera(Vector3 origin, float delta){
-		Vector3 cameraDir = GetDirection();
-		RaycastHit hit;
-		Vector3 point = Vector3.zero;
-		if (Physics.Raycast(origin, cameraDir, out hit, radius) && collide){
-			cameraPos = hit.point - cameraDir.normalized;
-		}else
-			cameraPos = origin + cameraDir * radius;
+        return matrix.MultiplyPoint(point);
+    }
 
-		transform.position = Vector3.Lerp(transform.position, cameraPos, delta * lerpVelocity);
-		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(mouse.y, mouse.x, 0)), delta*lerpVelocity);
-	}
+    public Vector3 GetDirection()
+    {
+        Matrix4x4 matrix =
+            Matrix4x4.Rotate(Quaternion.Euler(new Vector3(0, mouse.x, 0)))
+            * Matrix4x4.Rotate(Quaternion.Euler(new Vector3(mouse.y, 0, 0)))
+            * Matrix4x4.Translate(-Vector3.forward)
+            * Matrix4x4.identity;
 
-	void PositionCamera(Transform obj, float delta)
-	{
-		ForcePositionCamera(obj.position, delta);
-	}
+        return matrix.MultiplyPoint(new Vector3(0, 0, 0));
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (obj == null) return;
+        Vector3 cameraDir = GetDirection();
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(obj.position, obj.position + cameraDir * radius);
+    }
+
+    public void ForcePositionCamera(Vector3 origin, float delta)
+    {
+        Vector3 cameraDir = GetDirection();
+        RaycastHit hit;
+        Vector3 point = Vector3.zero;
+        if (Physics.Raycast(origin, cameraDir, out hit, radius) && collide)
+        {
+            cameraPos = hit.point - cameraDir.normalized;
+        }
+        else
+            cameraPos = origin + cameraDir * radius;
+
+        transform.position = Vector3.Lerp(transform.position, cameraPos, delta * lerpVelocity);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(mouse.y, mouse.x, 0)), delta * lerpVelocity);
+    }
+
+    void PositionCamera(Transform obj, float delta)
+    {
+        ForcePositionCamera(obj.position, delta);
+    }
+    public void CameraRotate(float side)
+    {
+        mouse.x += side * angle;
+        mouse.x %= 360;
+    }
 }
--- Assets/Scripts/GUI/UIManager.cs
+++ Assets/Scripts/GUI/UIManager.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class UIManager : MonoBehaviour
 {
     public GameObject mainPanel;
+    public GameObject gamePanel;
+
     public Button btnExit;
     public Button btnContinue;
     public Button btnNewGame;
     public Button btnSettings;
+    public Button btnMenu;
+    public Button btnBack;
+    public Button btnLeft;
+    public Button btnRight;
     private bool active = true;
 
     void Start()
@@ -16,15 +22,19 @@ public class UIManager : MonoBehaviour
         Button bNewGame = btnNewGame.GetComponent<Button>();
         Button bSettings = btnSettings.GetComponent<Button>();
         Button bContinue = btnContinue.GetComponent<Button>();
+        Button bMenu = btnMenu.GetComponent<Button>();
+        Button bLeft = btnLeft.GetComponent<Button>();
+        Button bRight = btnRight.GetComponent<Button>();
+        // Button bBack = btnBack.GetComponent<Button>();
 
         bExit.onClick.AddListener(ClickExit);
         bContinue.onClick.AddListener(ClickContinue);
         bNewGame.onClick.AddListener(ClickNewgame);
         bSettings.onClick.AddListener(ClickSettings);
+        bMenu.onClick.AddListener(ToggleMenu);
     }
     public void ClickContinue()
     {
-        mainPanel.SetActive(false);
     }
     public void ClickNewgame()
     {
@@ -37,8 +47,9 @@ public class UIManager : MonoBehaviour
     public void ClickSettings()
     {
     }
-
-    public void ToggleMenu(){
+    public void ToggleMenu()
+    {
+        gamePanel.SetActive(active);
         active = !active;
         mainPanel.SetActive(active);
     }
